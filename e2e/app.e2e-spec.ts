dfa994becd64d46564ba9c9f0b55891519f731e4
Michael Simmons <github@e7i.org>
2017-04-02 17:15:05 -0500

    Initial setup

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { PublicationStudioPage } from './app.po';
 
 describe('publication-studio App', () => {
-  let page: PublicationStudioPage;
+    let page: PublicationStudioPage;
 
-  beforeEach(() => {
-    page = new PublicationStudioPage();
-  });
+    beforeEach(() => {
+        page = new PublicationStudioPage();
+    });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('pstudio works!');
-  });
+    it('should display message saying app works', () => {
+        page.navigateTo();
+        expect(page.getParagraphText()).toEqual('pstudio works!');
+    });
 });
