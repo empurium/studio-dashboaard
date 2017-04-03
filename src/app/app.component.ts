399db8a6b35e011ac613e23a2ab15044d822e12e
Michael Simmons <github@e7i.org>
2017-04-02 23:34:39 -0500

    AppComponent: handle authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '@freescan/skeleton';
 
 
 @Component({
     selector: 'pstudio-root',
     template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor(private authentication: AuthenticationService) {
+    }
+
+    /**
+     * Attempt to login the user. Essentially checks the URL for an access_token,
+     * saves to local storage, and then removes it from the URL.
+     * Performed in AppComponent to remove from the URL as early as possible.
+     */
+    public ngOnInit(): void {
+        this.authentication.attemptLogin();
+    }
 }
