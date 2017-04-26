ca708872602e6070019fa252aa286a58f18913bb
Michael Simmons <github@e7i.org>
2017-04-25 21:43:36 -0500

    Articles: Tier Resource management

--- src/app/articles/article/article.component.ts
+++ src/app/articles/article/article.component.ts
@@ -7,16 +7,17 @@ import { Observable } from 'rxjs';
 import {
     AuthenticationService,
     AlertService,
-    PeopleService,
     Person,
+    PeopleService,
     PeopleResponse,
-    ArticleService,
     Article,
+    ArticleService,
     ArticleResponse,
-    TierService,
     Tier,
-    TierResourceService,
+    TierService,
     TierResource,
+    TierResourceService,
+    TierResourceResponse,
 } from '@freescan/skeleton';
 
 
@@ -27,10 +28,11 @@ import {
     encapsulation: ViewEncapsulation.None,
 })
 export class ArticleComponent implements OnInit {
-    public article: Article     = new Article();
-    public tiers: Tier[];
-    public tier: Tier;
     public people: Person[];
+    public tiers: Tier[];
+    public tierId: string;
+    public tierResources: TierResource[];
+    public article: Article     = new Article();
     public loading: boolean     = true;
     public saving: boolean      = false;
     public momentPublished: any = moment();
@@ -55,29 +57,60 @@ export class ArticleComponent implements OnInit {
     }
 
     /**
-     * Request the tiers for this white label so they can assign articles to them.
+     * Request the article if one was requested, otherwise this is a new article.
      */
-    public loadTiers(): void {
-        this.tierService.all().subscribe(
-            (tiers: Tier[]) => this.tiers = tiers,
-            (error: string) => this.alerts.warning(
-                null, 'There are no payment tiers available to assign to.',
-            ),
-        );
+    public loadArticle(): void {
+        this.route.params
+            .switchMap((params: Params) => {
+                this.loading = !!params.id;
+                if (params.id) {
+                    this.loadTierResources(params.id);
+                }
+
+                return params.id ? this.articleService.one(params.id) : Observable.empty();
+            })
+            .subscribe(
+                (response: ArticleResponse) => {
+                    this.loading = false;
+                    this.article = response.data;
+                    this.setMomentPublished();
+                },
+                (error: string) => this.alerts.error(null, 'Could not locate this article.'),
+            );
     }
 
     /**
      * Request the tiers for this white label so they can assign articles to them.
      */
-    public loadTierResources(): void {
-        this.tierResourceService.for(this.article.id).subscribe(
-            (tiers: TierResource[]) => {
-                // nothing yet
-            },
-            (error: string) => this.alerts.warning(
-                null, 'There are no payment tiers available to assign to.',
-            ),
-        );
+    public loadTiers(): void {
+        this.tierService
+            .all()
+            .subscribe(
+                (tiers: Tier[]) => {
+                    this.tiers = tiers;
+                    this.setTierId();
+                },
+                (error: string) => this.alerts.warning(
+                    null, 'There are no payment tiers available to assign to.',
+                ),
+            );
+    }
+
+    /**
+     * Request the Tier Resource for a given article.
+     */
+    public loadTierResources(articleId: string): void {
+        this.tierResourceService
+            .for(articleId)
+            .subscribe(
+                (tierResources: TierResource[]) => {
+                    this.tierResources = tierResources;
+                    this.setTierId();
+                },
+                (error: string) => this.alerts.warning(
+                    null, 'Error loading Payment Tier Resources for this publication.',
+                ),
+            );
     }
 
     /**
@@ -96,23 +129,6 @@ export class ArticleComponent implements OnInit {
     }
 
     /**
-     * Request the article if one was requested, otherwise this is a new article.
-     */
-    public loadArticle(): void {
-        this.route.params
-            .switchMap((params: Params) => {
-                this.loading = !!params.id;
-                return params.id ? this.articleService.one(params.id) : Observable.empty();
-            })
-            .subscribe((response: ArticleResponse) => {
-                this.loading = false;
-                this.article = response.data;
-                this.setMomentPublished();
-                this.loadTierResources();
-            });
-    }
-
-    /**
      * Either POST a new article or PUT the new contents.
      */
     public store(form: NgForm): void {
@@ -120,10 +136,11 @@ export class ArticleComponent implements OnInit {
         this.overwrite(form.form.value);
         this.setPublishedAt(this.article.published_at);
 
+        this.storeTierResource();
+
         if (!this.article.id) {
             this.setPersonId();
             this.post();
-
             return;
         }
 
@@ -131,20 +148,23 @@ export class ArticleComponent implements OnInit {
     }
 
     /**
-     * Store the Tier Resource if it was specified.
+     * Store the selected Tier Resource.
+     * TODO - support more than one Tier Resource at a time.
      */
     public storeTierResource(): void {
-        this.tierResourceService
-            .post(this.tier, this.article.id)
-            .finally(() => this.saving = false)
-            .subscribe(
-                (response: TierResource) => {
-                    // this.tier = response;
-                    this.alerts.success(null, 'Article has been saved.');
-                    this.router.navigate([`../edit/${this.article.id}`], { relativeTo: this.route });
-                },
-                (error: any) => this.alerts.errorMessage(error),
-            );
+        this.deleteTierResources();
+
+        let tier: Tier|false = this.getTier();
+        if (!tier) {
+            return;
+        }
+
+        if (!this.tierResources || !this.tierResources.length) {
+            this.postTierResources(tier);
+            return;
+        }
+
+        this.putTierResources(tier);
     }
 
     /**
@@ -152,6 +172,35 @@ export class ArticleComponent implements OnInit {
      */
     public cancel(): void {
         this.router.navigate(['publications']);
+    }
+
+    /**
+     * Get the Tier by Tier ID.
+     */
+    public getTier(): Tier|false {
+        let tier: Tier = _.find(this.tiers, { id: this.tierId });
+        return tier ? tier : false;
+    }
+
+    /**
+     * Get the Tier name by Tier ID for the template.
+     */
+    public getTierName(): string {
+        let tier: Tier = _.find(this.tiers, { id: this.tierId });
+        return tier ? tier.name : 'all';
+    }
+
+    /**
+     * Get the Tier Resource by Tier ID.
+     */
+    public getTierResource(): TierResource {
+        // We map() out the tier data so we can find() it by tierId
+        let tierResource: any = _.find(
+            _.map(this.tierResources, (t: TierResource) => t.tier ? t.tier.data : {}),
+            { id: this.tierId },
+        );
+
+        return tierResource;
     }
 
     /**
@@ -174,7 +223,7 @@ export class ArticleComponent implements OnInit {
      * if there isn't already one set on the article.
      */
     private setPersonId(): void {
-        const person: Person = _.find(this.people, { user_id: this.authentication.userId() });
+        let person: Person = _.find(this.people, { user_id: this.authentication.userId() });
 
         if (!person) {
             this.alerts.error(null, 'Please create an Author for your account before publishing.');
@@ -186,6 +235,16 @@ export class ArticleComponent implements OnInit {
         }
 
         this.article.person_id = person.id;
+    }
+
+    /**
+     * Set the Tier based on the Tier Resources this article is assigned to.
+     */
+    private setTierId(): void {
+        let tierId: any = _.get(_.first(this.tierResources), 'tier.data.id');
+        let tier: Tier  = _.find(this.tiers, { id: tierId });
+
+        this.tierId = tier ? tier.id : '';
     }
 
     /**
@@ -221,7 +280,67 @@ export class ArticleComponent implements OnInit {
     }
 
     /**
-     * Overwrites the values from the form to the Article model before submission.
+     * POST the Tier Resources for this article.
+     */
+    private postTierResources(tier: Tier): void {
+        this.tierResourceService
+            .post(tier, this.article.id)
+            .subscribe(
+                (response: TierResource): void => {
+                    this.alerts.success(null, `Saved to ${tier.name} payment tier.`);
+                },
+                (error: any): void => {
+                    this.alerts.error(null, `Could not save to ${tier.name} payment tier!`);
+                },
+            );
+    }
+
+    /**
+     * PUT the Tier Resource to update the tier_id accordingly.
+     */
+    private putTierResources(tier: Tier): void {
+        let tierResource: any = this.getTierResource();
+        if (!tierResource) {
+            return;
+        }
+
+        tierResource.tier_id = this.tierId;
+
+        this.tierResourceService
+            .put(tierResource)
+            .subscribe(
+                (response: TierResourceResponse): void => {
+                    this.alerts.success(null, `Saved to ${tier.name} payment tier.`);
+                },
+                (error: any): void => {
+                    this.alerts.error(null, `Could not save to ${tier.name} payment tier!`);
+                },
+            );
+    }
+
+    /**
+     * DELETE all existing Tier Resources if the article was marked as Free.
+     * Does not delete if there was no change.
+     */
+    private deleteTierResources(): void {
+        if (this.tierId === _.get(this.tierResources, 'tier.data.id')) {
+            return;
+        }
+
+        _.each(this.tierResources, (tierResource: TierResource) => {
+            this.tierResourceService
+                .delete(tierResource)
+                .subscribe(
+                    () => {
+                        this.tierResources = [];
+                        this.alerts.warning(null, 'Removed from payment tier.');
+                    },
+                );
+        });
+    }
+
+    /**
+     * Overwrites the values from the form to the Article model for submission.
      */
     private overwrite(values: Article): void {
         _.each(values, (value: any, key: string) => {
