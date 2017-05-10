34af230c914340cb50a9eb4ea219c2b265846cf2
Michael Simmons <github@e7i.org>
2017-05-10 15:46:14 -0500

    Studio Dashboard: containers for new fluid layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pstudio-home',
     template: `
-<div class="container mt-3">
+<div class="container mt-2">
     <router-outlet></router-outlet>
 </div>`,
 })
